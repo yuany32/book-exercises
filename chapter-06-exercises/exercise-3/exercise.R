cdcc01051186ec03c090223878d18c93d32f28d4
Claire Yuan <claireyuan99@gmail.com>
2020-01-17 12:57:02 -0800

    chapter 07 exercise-2 update

--- chapter-06-exercises/exercise-3/exercise.R
+++ chapter-06-exercises/exercise-3/exercise.R
@@ -2,17 +2,24 @@
 
 # Define a function `add_three` that takes a single argument and
 # returns a value 3 greater than the input
-
+add_three <- function(x) {
+  value <- x + 3
+  return(value)
+}
 
 # Create a variable `ten` that is the result of passing 7 to your `add_three` 
 # function
-
+ten <- add_three(7)
 
 # Define a function `imperial_to_metric` that takes in two arguments: a number 
 # of feet and a number of inches
 # The function should return the equivalent length in meters
-
+imperial_to_metric <- function(num_ft,num_in) {
+  num_m <- num_ft * 0.3 + num_in * 0.025
+  return(num_m)
+}
 
 # Create a variable `height_in_meters` by passing your height in imperial to the
 # `imperial_to_metric` function
+height_in_meters <- imperial_to_metric(5,7)
 
--- chapter-07-exercises/exercise-2/exercise.R
+++ chapter-07-exercises/exercise-2/exercise.R
@@ -2,33 +2,34 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first_ten <- c(10:20)
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next_ten <- seq(21,30)
 
 # Create a vector `all_numbers` by combining the previous two vectors
-
+all_numbers <- c(first_ten,next_ten)
 
 # Create a variable `eleventh` that contains the 11th element in `all_numbers`
-
+eleventh <- all_numbers[11]
 
 # Create a vector `some_numbers` that contains the 2nd through the 5th elements 
 # of `all_numbers`
+some_numbers <- all_numbers[2:5]
 
-
-
+?lapply
 # Create a vector `even` that holds the even numbers from 1 to 100
-
-
+numbers <- c(1:100)
+filter <- c(1:100) %% 2 == 0
+eleven <- numbers[filter == TRUE]
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-
+all(eleven %% 2 == 0)
 
 
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
-
+phone_numbers()
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
 
