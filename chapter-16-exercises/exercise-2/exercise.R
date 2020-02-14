354bd3e7642c90842439a4fd6303f4ce57e0607f
Claire yuan <claireyuan99@gmail.com>
2020-02-13 16:03:10 -0800

    edit chapter 16,18

--- chapter-16-exercises/exercise-2/exercise.R
+++ chapter-16-exercises/exercise-2/exercise.R
@@ -43,21 +43,30 @@ library("ggplot2")
 # For another version, draw the same plot but with `violin` geometry instead of 
 # `boxplot` geometry!
 # How does the logarithmic scale change the data presentation?
-
+ggplot(data = diamonds_sample) + 
+  geom_violin(mapping = aes(x = color, y = price)) +
+  scale_y_log10()
 
 # Another interesting plot: draw a plot of the diamonds price (y) by carat (x), 
 # using a heatmap of 2d bins (geom_bin2d)
 # What happens when you make the x and y channels scale logarithmically?
+ggplot(data = diamonds_sample) + 
+  geom_bin2d(mapping = aes(x = carat, y = price)) +
+  scale_y_log10() +
+  scale_x_log10()
 
 
 # Draw a scatter plot for the diamonds price (y) by carat (x). Color each point
 # by the clarity (Remember, this will take a while. Use a sample of the diamonds 
 # for faster results)
-
+ggplot(data = diamonds_sample) + 
+  geom_point(mapping = aes(x = carat, y = price, color = clarity))
 
 # Change the color of the previous plot using a ColorBrewer scale of your choice. 
 # What looks nice?
-
+ggplot(data = diamonds_sample) + 
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  scale_color_brewer(palette = "Purples")
 
 
 ## Coordinate Systems
@@ -66,7 +75,9 @@ library("ggplot2")
 # For best results, SET the `width` of the geometry to be 1 (fill plot, no space
 # between)
 # TIP: You can save the plot to a variable for easier modifications
-
+ggplot(data = diamonds_sample) + 
+  geom_point(mapping = aes(x = carat, y = price, color = clarity)) +
+  scale_color_brewer(palette = "Purples", direction = "-1")
 
 # Draw the same chart, but with the coordinate system flipped
 
@@ -79,7 +90,9 @@ library("ggplot2")
 
 # Take the scatter plot of price by carat data (colored by clarity) and add 
 # _facets_ based on the diamond's `color`
-
+ggplot(data = diamonds_sample) +
+  geom_point(mapping = aes(x = carat, y = price, color = color)) +
+  facet_wrap(~clarity)
 
 
 ## Saving Plots
--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -7,10 +7,11 @@ library("jsonlite")
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
+source("apikey.R") #go to that file, select all, and hit ctrl-enter
 
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,12 +21,20 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+endpoint <- "reviews/search.json"
+uri <- paste0(base_uri,endpoint)
 
+#access with api key and movie name as search parameter
+query_params <- list("api.key" = nyt_key, "movie_name" = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
-
+response <- GET(uri, query = query_params)
+response
+body <- content(response,"text",encoding = "UTF-8")
+data <- fromJSON(body)
+data
 # What kind of data structure did this produce? A data frame? A list?
 
 
